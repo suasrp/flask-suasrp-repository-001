4c8b11ae0722ed4b8ba20fbb7972c4dcf4809ebf
suasrp <suasrp@gmail.com>
2024-11-05 13:39:36 -0600

    Update app.py # FROM sp_ul_flask_t05d_f 11-05-2024
    # FROM sp_ul_flask_t05d_f 11-05-2024

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-# LAST ONE from ORIGINAL flask-suasrp-repository-001 11-05-2024
+# FROM sp_ul_flask_t05d_f 11-05-2024
 #
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash
 from gtts import gTTS
-import os
-import nltk
-
-nltk.download('wordnet')
+import random
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Change this to a secure random key
+app.secret_key = 'your_secret_key'  # Change this to a random secret key for production
 
+# Predefined list of words
 words = [
-    # Your list of words here...
+    "abbreviate", "abnormality", "abode", "abrasion", "abundantly", "academic",
+    "accessory", "accordion", "acidic", "acne", "acrobat", "adhesive",
+    "admirable", "adoption", "adversary", "affected", "affliction", "affordable",
+    "agenda", "airport", "alimony", "allergic", "alliance", "alpaca",
+    "alphabetical", "amateur", "amplify", "amusing", "animate", "anklebone",
+    "annex", "antibacterial", "antibiotic", "anxiety", "apparition", "appease",
+    "applause", "aptitude", "aquamarine", "arcade", "arrangement", "assortment",
+    "athletic", "attractive", "auditory", "avalanche", "avocado", "badminton",
+    "balky", "Ballyhoo", "barbarian", "bareback", "bargain", "barrette",
+    "visitation", "vitality", "vivid", "vocation", "volcanic", "volume",
+    "waistband", "wallaby", "warehouse", "warrant", "wash-and-wear", "waspish",
+    "wearable", "web-footed", "wharf", "wheelchair", "wherefore", "white blood cell",
+    "whitening", "wireless", "wisecrack", "wittingly", "woozy", "workmanship",
+    "xylophone", "yacht", "yearling", "zealous", "zestfully"
 ]
 
 # Create 26 tests (A-Z)
@@ -25,51 +36,39 @@ def create_tests(words_list):
 tests = create_tests(words)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/select_test')
 def select_test():
     return render_template('select_test.html', tests=tests)
 
-@app.route('/start_test/<letter>')
+@app.route('/start_test/<letter>', methods=['GET', 'POST'])
 def start_test(letter):
-    word_list = tests.get(letter, [])
-    return render_template('test.html', letter=letter, words=word_list)
+    if request.method == 'POST':
+        # Handle answer submission
+        user_input = request.form['user_input'].strip().lower()
+        correct_word = request.form['correct_word']
+        if user_input == correct_word:
+            flash('Correct!', 'success')
+        else:
+            flash(f'Incorrect. The correct spelling is: {correct_word}', 'danger')
+        return redirect(url_for('start_test', letter=letter))
 
-@app.route('/check_spelling', methods=['POST'])
-def check_spelling():
-    user_input = request.form['spelling'].strip().lower()
-    current_word = request.form['current_word']
-    score = int(request.form['score'])
-
-    if user_input == current_word:
-        score += 1
-        result_message = "Correct!"
-    else:
-        result_message = "Incorrect!"
-
-    return redirect(url_for('test_result', result=result_message, score=score, current_word=current_word))
-
-@app.route('/test_result')
-def test_result():
-    result = request.args.get('result')
-    score = request.args.get('score')
-    current_word = request.args.get('current_word')
-    return render_template('result.html', result=result, score=score, current_word=current_word)
+    words_to_test = tests[letter]
+    current_word = random.choice(words_to_test)  # Randomly select a word for the test
+    return render_template('test.html', letter=letter, current_word=current_word)
 
 @app.route('/pronounce/<word>')
 def pronounce(word):
     tts = gTTS(text=word, lang='en')
-    filename = f'temp_{word}.mp3'
+    filename = 'static/temp_word.mp3'
     tts.save(filename)
+    return redirect(url_for('play_sound', filename='temp_word.mp3'))
 
-    # Serve the audio file from a temporary location
-    return redirect(url_for('select_test', audio_file=filename))
-
-@app.route('/audio/<filename>')
-def audio(filename):
-    return send_from_directory(os.getcwd(), filename)
+@app.route('/play_sound/<filename>')
+def play_sound(filename):
+    return render_template('play_sound.html', filename=filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
